5471f891fa8bb2a81016b792509536dd3190e38c
davutseker29 <davutseker29@gmail.com>
2020-05-27 13:41:55 -0700

    TestNG Intro

---
+++ src/com/testNG/class01/DifferentAnnotations.java
@@ -0,0 +1,48 @@
+package com.testNG.class01;
+
+import org.testng.annotations.*;
+public class DifferentAnnotations {
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before Suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After Suite");
+	}
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After Class");
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method");
+	}
+	
+	@Test
+	public void testMethod() {
+		System.out.println("Actual Test is running...");
+	}
+
+}
