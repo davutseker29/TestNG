88923242a1e797ff4b2064ae778bcb6c0c8a3bb4
dSeker <davutseker29@gmail.com>
2021-08-08 16:32:18 -0700

    added

--- src/com/class01/TestNGDemo.java
+++ src/com/class01/TestNGDemo.java
@@ -3,24 +3,32 @@ package com.class01;
 import org.testng.annotations.Test;
 
 public class TestNGDemo {
-		
+
 	@Test
 	public void testOne() {
-		
+
 		System.out.println("Test 1");
-		
+
 	}
+
 	@Test
 	public void testTwo() {
-		
+
 		System.out.println("Test 2");
-		
+
 	}
+
 	@Test
 	public void testThree() {
-		
+
 		System.out.println("Test 3");
-		
+
+	}
+
+	@Test
+	public void testFour() {
+
+		System.out.println("Test 4");
 	}
 
 }
---
+++ src/com/class01/TestNGDemo01.java
@@ -0,0 +1,14 @@
+package com.class01;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo01 {
+	
+	@Test
+	
+	public void testOne() {
+		
+		System.out.println("Test 1");
+	}
+
+}
